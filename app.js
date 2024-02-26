7ead3ccee21fe1e8ef5b461ee3724d4a3e7ed39e
andrew1k3 <andrew.jgardner17@gmail.com>
2024-02-26 16:59:48 +1100

    done mobile design

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const splash = document.getElementById("splash");
 const title = document.getElementById("title");
-const damp = 0.5;
+const damp = 0.8;
 
 window.addEventListener("mousemove", (e) => {
   let xPercentage = e.x / e.view.innerWidth;
-  console.log(xPercentage);
-  splash.style.backgroundPosition = `${45 - (0.5 - xPercentage * damp)}%`;
+  let yPercentage = e.y / e.view.innerHeight;
+  splash.style.backgroundPosition = `${45 - (0.5 - xPercentage * damp)}% ${
+    45 - (0.5 - yPercentage * damp)
+  }%`;
 });
